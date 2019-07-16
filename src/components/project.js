dc0a094d4e18542c9d7e17294d97cfc83fbe2601
James Firkins <arceihn@gmail.com>
2019-07-16 18:52:54 +0930

    :package: Added project images

--- src/components/project.js
+++ src/components/project.js
@@ -1,11 +1,16 @@
 import React from "react"
+import cover from "../images/gatsby-astronaut.png"
 
 const Project = (props) => {
+  const coverStyle = {
+    backgroundImage: `url(${cover})`
+  }
+
   return (
     <div className="row-outer">
       <article className="row">
         <a href={props.url} className="block-link">g</a>
-        <div href={props.url} className={`cover ${props.slug}`}></div>
+        <div href={props.url} className={`cover ${props.slug}`} style={coverStyle}></div>
         <div className="copy">
           <h1><a href={props.url}>{props.title}</a></h1>
           <p>{props.description}</p>
