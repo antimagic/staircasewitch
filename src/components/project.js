9eb05fe2fe9dec32624ed41f2a2bfe31ae815cf9
James Firkins <arceihn@gmail.com>
2019-07-16 18:53:53 +0930

    :sparkles: Query image publicUrl based on slug

--- src/components/project.js
+++ src/components/project.js
@@ -1,15 +1,32 @@
 import React from "react"
-import cover from "../images/gatsby-astronaut.png"
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Project = (props) => {
+  const images = useStaticQuery(graphql`
+    query ProjectImages {
+      allFile(filter: {name: {regex: "/project/"}}) {
+        nodes {
+          publicURL
+          name
+        }
+      }
+    }
+  `).allFile.nodes
+
+  const cover = images.filter(val => {
+    return val.name === `project-${props.slug}`
+  })[0]
+
   const coverStyle = {
-    backgroundImage: `url(${cover})`
+    backgroundImage: `url(${cover.publicURL})`
   }
+
+  
 
   return (
     <div className="row-outer">
       <article className="row">
-        <a href={props.url} className="block-link">g</a>
+        <a href={props.url} className="block-link">{props.title}</a>
         <div href={props.url} className={`cover ${props.slug}`} style={coverStyle}></div>
         <div className="copy">
           <h1><a href={props.url}>{props.title}</a></h1>
--- src/components/projects.js
+++ src/components/projects.js
@@ -12,7 +12,6 @@ const Projects = () => {
             slug
             description
             url
-            image
           }
         }
       }
